ebaeb27a91ce5877abe4dce9debe08b6f6f3d87a
VladChernenko <vladchernenko@protonmail.com>
2025-12-03 23:36:56 +0100

    Small update

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -9,7 +9,9 @@ import (
 var GENERATION_THREAD_METADATA = struct {
 	sync.RWMutex
 	Handlers map[string]*structures.GenerationThreadMetadataHandler
-}{Handlers: make(map[string]*structures.GenerationThreadMetadataHandler)}
+}{
+	Handlers: make(map[string]*structures.GenerationThreadMetadataHandler),
+}
 
 var APPROVEMENT_THREAD_METADATA = struct {
 	RWMutex sync.RWMutex
